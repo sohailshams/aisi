52aa3eb3af6bf2a58882c5e52f576626c82386b1
sohailshams <sohailshamas@yahoo.com>
2023-01-07 11:38:26 +0000

    Added Navbr component - created & styled navbar, added google fonts and Outlet.

---
+++ src/routes/navbar/Navbar.jsx
@@ -0,0 +1,32 @@
+import { Outlet, Link } from 'react-router-dom';
+import { Fragment } from 'react';
+
+const Navbar = () => {
+
+    return (
+      <Fragment>
+        <nav className='flex justify-between pl-5 pr-5 py-7 font-semi-bold font-[Pacifico] text-lg'>
+            <Link to='/'>
+                <h2>aisi.</h2>
+            </Link>
+            <div className='space-x-4'>
+                <Link>
+                    shop
+                </Link>
+                <Link>
+                    contact
+                </Link>
+                <Link>
+                    sign in
+                </Link>
+                <Link>
+                    cart
+                </Link>
+            </div>
+        </nav>
+        <Outlet />
+      </Fragment>
+    )
+  }
+
+export default Navbar;
