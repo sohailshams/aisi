5831aba8235ac032ea740eeb64eda94489129a49
sohailshams <sohailshamas@yahoo.com>
2023-01-07 10:34:28 +0000

    Installed react-router-dom and setup home page route, also updated tailwind config file.

--- src/App.js
+++ src/App.js
@@ -1,34 +1,14 @@
-import Category from "./components/category-item/Category";
-import Directory from "./components/directory/Directory";
+import { Routes, Route } from 'react-router-dom';
+import Home from './routes/home/Home'
 
 function App() {
-  const categories = [
-    {
-      id: '1',
-      "name": "Hats",
-      "imageUrl": "./images/hats-img.png"
-    }, {
-      id: '2',
-      "name": "Jackets",
-      "imageUrl": "./images/jackets-img.png"
-    }, {  
-      id: '3',
-      "name": "Sneakers",
-      "imageUrl": "./images/sneakers-img.png"
-    }, {
-      id: '4',
-      "name": "Womens",
-      "imageUrl": "./images/womens-img.png"
-    }, {
-      id: '5',
-      "name": "Mens",
-      "imageUrl": "./images/men-img.png"
-    }
-  ]
 
   return (
-    <Directory categories={categories} />
-  );
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  )
+
 }
 
 export default App;
