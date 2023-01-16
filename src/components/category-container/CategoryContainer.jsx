4736d551fdb91ff5f3fdb9919e002fd70582517d
sohailshams <sohailshamas@yahoo.com>
2023-01-16 17:18:12 +0000

    Added CategoryContainer component to render only four items of each category.

---
+++ src/components/category-container/CategoryContainer.jsx
@@ -0,0 +1,19 @@
+import ProductCard from "../product-card/ProductCard";
+
+
+const CategoryContainer = ({title, products}) => {
+  return (
+    <div>
+        <h2 className='text-2xl font-bold pl-8 ml-2 my-4'><span>{title.toUpperCase()}</span></h2>
+        <div className='grid max-[500px]:grid-cols-1 grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-x-4 gap-y-12 px-[40px] py-[20px] mb-10'>
+            {
+                products.filter((_, index ) => index < 4).map((product) => (
+                    <ProductCard key={product.id} product={product} />
+                ))
+            }
+        </div>
+    </div>
+  )
+}
+
+export default CategoryContainer;
--- src/routes/shop/Shop.jsx
+++ src/routes/shop/Shop.jsx
@@ -1,6 +1,6 @@
 // import SHOP_DATA from '../../shop-data.json';
 import { useContext, Fragment } from 'react';
-import ProductCard from '../../components/product-card/ProductCard';
+import CategoryContainer from '../../components/category-container/CategoryContainer';
 
 import { CategoriesContext } from '../../contexts/categories.context';
 
@@ -9,16 +9,14 @@ const Shop = () => {
 
   return (
       <Fragment>
-        {Object.keys(categoriesMap).map((title) => (
-          <Fragment key={title}>
-            <h2 className='text-2xl font-bold pl-8 ml-2 my-4'>{title.toUpperCase()}</h2>
-            <div className='grid max-[500px]:grid-cols-1 grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-x-4 gap-y-12 px-[40px] py-[20px]'>
-                {categoriesMap[title].map((product) => (
-                  <ProductCard key={product.id} product={product} />
-                ))}
-            </div>
-          </Fragment>
-        ))}
+        {Object.keys(categoriesMap).map((title) => {
+          // Get products from the categoriesMap
+          const products = categoriesMap[title];
+
+          return (
+            <CategoryContainer key={title} title={title} products={products} />
+          )
+        })}
       </Fragment>
   );
 };
