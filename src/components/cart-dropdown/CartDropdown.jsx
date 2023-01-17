ebb1e11539fd751381beacea24f92a9aceb4c7c6
sohailshams <sohailshamas@yahoo.com>
2023-01-17 11:07:27 +0000

    Added logic to render empty cart message and checkout button conditionally.

--- src/components/cart-dropdown/CartDropdown.jsx
+++ src/components/cart-dropdown/CartDropdown.jsx
@@ -19,9 +19,20 @@ const CartDropdown = () => {
     <div>
         <div className='absolute w-[240px] h-[340px] border-2 border-black top-20 right-4 z-40 bg-white flex flex-col overflow-y-auto'>
             <div className="ml-4 mb-4">
-                {cartItems.map(item => <CartItem key={item.id} cartItem={item} />)}
+                { cartItems.length ? (
+                  cartItems.map(item => <CartItem key={item.id} cartItem={item} />)
+                  ) : (
+                    <p className="pl-10 absolute top-[40%] font-semibold">Your cart is empty</p> 
+                    )
+                
+              }
             </div>
-            <button className='bg-black py-2 px-2 text-slate-50 opacity-75 hover:bg-slate-50 hover:text-black drop-shadow-2xl ml-2 mr-2 mt-auto mb-2' onClick={navigateToCheckHandler}>Checkout</button>
+            { cartItems.length ? (
+              <button className='bg-black py-2 px-2 text-slate-50 opacity-75 hover:bg-slate-50 hover:text-black drop-shadow-2xl ml-2 mr-2 mt-auto mb-2' onClick={navigateToCheckHandler}>Checkout</button>
+                ) : (
+                    ''
+                  )
+            }
         </div>
     </div>
   )
